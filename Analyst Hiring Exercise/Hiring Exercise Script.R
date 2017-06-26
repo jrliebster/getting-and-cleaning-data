e9ba6aa19820843af22a256e47ac0bf65754f38c
Jules Liebster <jules.liebster@tntp.org>
2017-06-26 15:56:49 -0400

    updates

--- Analyst Hiring Exercise/Hiring Exercise Script.R
+++ Analyst Hiring Exercise/Hiring Exercise Script.R
@@ -1,51 +1,45 @@
-#Business Rules:
-#Values of "<10" may be omitted (i.e., treated as NA)
-#Ensure that every school on the Student Scores list has a match on the Educator Effectiveness Snapshot list
+# Business Rules:
+# Values of "<10" may be omitted (i.e., treated as NA)
+# Ensure that every school on the Student Scores list has a match on the Educator Effectiveness Snapshot list
 
-#load packages
-library(pacman, scales, fuzzyjoin)
-p_load(readxl, readr, dplyr, janitor, tidyr, stringr, ggplot2, data.table, knitr, scales, psych, Hmisc, corrplot, gtools, stats)
+# load packages
+library(pacman)
+p_load(readr, dplyr, janitor, tidyr, stringr, ggplot2, data.table, corrplot)
 
-#read data with clean names
-student_scores <- read_csv ("studentscores.csv")%>%
+# read data with clean names
+student_scores <- read_csv ("StudentScores.csv") %>%
   clean_names()
-educator_effectiveness <- read_csv ("educatoreffectiveness.csv", col_names = TRUE, skip = 7)
 
-#delete rows with missing data/values of less than 10
+# revert file names back to original!!
+educator_effectiveness <- read_csv ("EducatorEffectivenessSnapshot.csv", col_names = TRUE, skip = 7) %>%
+  clean_names()
+
+# delete rows with missing data/values of less than 10
 student_scores <-filter (student_scores, number_tested !=  "< 10")
 educator_effectiveness <- educator_effectiveness %>%
   filter(!is.na(total))
 
-#ensure all numbers are read as numeric variables
+# ensure all numbers are read as numeric variables
+# Look at RR code review to shorten this
 student_scores <- student_scores %>% 
-  mutate_at(vars(starts_with("level")),funs(as.numeric)) %>%   
-  mutate_at(vars(starts_with("number")),funs(as.numeric)) %>%
-  mutate_at(vars(contains("percent")),funs(as.numeric)) %>%
-  mutate_at(vars(contains("average")),funs(as.numeric))
+  mutate_at(vars(number_tested:average_scaled_score), funs(as.numeric(.)))
 
-#Ensure that every school on the Student Scores list has a match on the Educator Effectiveness Snapshot list
-#same number of observations when joined, with no missing data
-student_scores$building_name %in% educator_effectiveness$location
-#not all return "TRUE", so not every student score location has a match on educator effectiveness snapshot
-
-
-#Part 1: Summarize the Student Scores data to show the percent of students who are proficient or higher in math at each school.   
-#a.	Populate a table showing the top ten schools in math proficiency along with their proficiency rates (percent of students scoring at proficient or higher). There should be one percentage per school that accounts for all students at the school.  
-#add percent in levels 1 and 2, mutliply that percent by number of students tested, group by school name, then calculate weighted average of proficiency/number proficient
+# Part 1: Summarize the Student Scores data to show the percent of students who are proficient or higher in math at each school.   
+  # a.	Populate a table showing the top ten schools in math proficiency along with their proficiency rates (percent of students scoring at proficient or higher). There should be one percentage per school that accounts for all students at the school.
 math_proficiency <- student_scores %>% 
   filter(subject_name == "Mathematics", subgroup == "All Students") %>%
-  select(building_name, subject_name, number_tested, level1_highlyproficient, level2_proficient) %>%
-  mutate(percent_proficient_or_higher = level1_highlyproficient + level2_proficient) %>%
-  mutate(number_proficient = number_tested*(percent_proficient_or_higher/100)) %>%
+  select(building_name, subject_name, number_tested, percent_proficient) %>%
+  mutate(number_proficient = number_tested*percent_proficient) %>%
   group_by(building_name) %>% 
-  summarise(school_math_proficiency_percent = weighted.mean(percent_proficient_or_higher, number_proficient))
+  summarise(school_math_proficiency_percent = weighted.mean(percent_proficient, number_proficient))
 
-#Douglass Academy showing as NaN, as they had 0 students at levels 1/2
+# DELETE AFTER REVIEW
+# Douglass Academy showing as NaN, as they had 0 students at levels 1/2
 math_proficiency$school_math_proficiency_percent[math_proficiency$school_math_proficiency_percent=="NaN"] <- "0"
 
-math_proficiency$school_math_proficiency_percent <- as.numeric(as.character(math_proficiency$school_math_proficiency_percent))
+# math_proficiency$school_math_proficiency_percent <- as.numeric(as.character(math_proficiency$school_math_proficiency_percent))
   
-#keep full list of schools in sep vector to complete part two, create new table for top 10  
+# keep full list of schools in sep vector to complete part two, create new table for top 10  
 top_math_proficiency <- math_proficiency %>%
   arrange(desc(school_math_proficiency_percent)) %>%
   slice(1:10)
@@ -57,19 +51,20 @@ bottom_math_proficiency <- math_proficiency %>%
 write_csv(top_math_proficiency, "mathproficiency.csv")
 
 
-#Part 2:  Explore the relationship between math proficiency and educator effectiveness at the school level.
-#a.	Calculate the correlation between the percent of students at a school who are proficient in math (the result of Part 1) and the percent of educators at a school who are rated Effective or Highly Effective.  All schools in the Student Scores file should be used in this correlation.
-#b.	The client for this work is the district's Director of School Improvement.  Provide the Director with a brief (no more than a paragraph or two) interpretation of the result of this correlation. What did you find? What do you think the district can learn from this result?   
+# Part 2:  Explore the relationship between math proficiency and educator effectiveness at the school level.
+# a.	Calculate the correlation between the percent of students at a school who are proficient in math (the result of Part 1) and the percent of educators at a school who are rated Effective or Highly Effective.  All schools in the Student Scores file should be used in this correlation.
+# b.	The client for this work is the district's Director of School Improvement.  Provide the Director with a brief (no more than a paragraph or two) interpretation of the result of this correlation. What did you find? What do you think the district can learn from this result?   
 
-#first, calculate % of educators, by school, rated HE/E
 
-educator_proficiency <- educator_effectiveness %>%
-  select(location, highly_effective_percent, highly_effective, effective_percent, effective)%>%
-  mutate(most_effective_percent = highly_effective_percent + effective_percent)
+# Ensure that every school on the Student Scores list has a match on the Educator Effectiveness Snapshot list
+# same number of observations when joined, with no missing data
+student_scores$building_name %in% educator_effectiveness$location
+# not all return "TRUE", so not every student score location has a match on educator effectiveness snapshot
 
 math_proficiency <- math_proficiency %>%
   rename(location = building_name)
 
+# Change names of schools on math_proficiency to match educator_effectiveness
 math_proficiency$location[math_proficiency$location=="Pulaski Elementary-Middle School"] <- "Pulaski Elem-Middle School"
 math_proficiency$location[math_proficiency$location=="Carstens Elementary-Middle School"] <- "Carstens Elem-Middle School"
 math_proficiency$location[math_proficiency$location=="All Buildings"] <- "Detroit City School District"
@@ -102,7 +97,9 @@ math_and_effectiveness_corrplot <- corrplot(cor(math_and_effectiveness_corr),
                                 sig.level=0.05, 
                                 method="number")
 
-#check that the weak, positive relationship reflected in corrplot is correct 
+# alternatively, if just need corr coefficient and p-value, can use cor.test(math_and_effectiveness$school_math_proficiency_percent, math_and_effectiveness$effective_or_more_percent)
+
+# visualize the relatinship between effectiveness and proficiency in a more straightforward way for client
 mod1 = lm(math_and_effectiveness$effective_or_more_percent ~ math_and_effectiveness$school_math_proficiency_percent) 
 summary(mod1)
 ggplot(math_and_effectiveness, aes(x = school_math_proficiency_percent, y = effective_or_more_percent)) +
@@ -112,17 +109,17 @@ ggplot(math_and_effectiveness, aes(x = school_math_proficiency_percent, y = effe
   labs(x = "Math Proficiency", y = "Teacher Effectiveness") +
   theme_light()
 
-#check the mean, as well as number of teachers rated effective/highly effective to see the spread
+# check the mean, as well as number of teachers rated effective/highly effective to see the spread
 mean(math_and_effectiveness$effective_or_more_percent)
 
 
-#Part 3:  Examine the spread of educator ratings in the district. The district is interested in how the evaluation rating scale has been used in practice.
-#a.	Create a chart illustrating a main message that you see in the ratings data.  The audience for this chart is the Director of School Improvement. 
-#b.	Provide a short accompanying description for your chart.  What trend(s) do you notice and how might they be actionable for the district?
+# Part 3:  Examine the spread of educator ratings in the district. The district is interested in how the evaluation rating scale has been used in practice.
+# a.	Create a chart illustrating a main message that you see in the ratings data.  The audience for this chart is the Director of School Improvement. 
+# b.	Provide a short accompanying description for your chart.  What trend(s) do you notice and how might they be actionable for the district?
 
-#subset educator effectiveness ratings for just Detroit City School District (overall dist ratings)
-#convert to long format
-#plot
+# subset educator effectiveness ratings for just Detroit City School District (overall dist ratings)
+# convert to long format
+# plot
 
 district_ratings <- subset(educator_effectiveness, subset = location=="Detroit City School District")
 district_ratings <- district_ratings %>%
