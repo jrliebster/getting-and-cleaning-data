ffdd864c483e5a6d9b92fd84a5990ddc4dd73eb0
Jules Liebster <jules.liebster@tntp.org>
2017-08-14 15:57:03 -0400

    coursera prog assignment data loaded, columns named, and merged to one dataset

--- Coursera/courseraweek2progassignment.R
+++ Coursera/courseraweek2progassignment.R
@@ -10,22 +10,35 @@
 library(pacman)
 p_load(readr, dplyr, utils, stringr, janitor, tidyr, data.table)
 
-# merge the train and test data sets 
-# 6 total to load; xtrain, xtest, subjecttest, subjecttrain, ytrain, ytest
+# 6 total datasets to load; xtrain, xtest, subjecttest, subjecttrain, ytrain, ytest
 # all are .txt files
+# col names for: 
+   # subject datasets are "sub_id"
+   # y datasets are "activity_id"
+   # x datasets are colnames from "features"
+   # activity label is "activity_type"
 
-subject_train = read.table('./train/subject_train.txt',header=FALSE)
-x_train = read.table('./train/x_train.txt',header=FALSE)
-y_train = read.table('./train/y_train.txt',header=FALSE)
-
-subject_test = read.table('./test/subject_test.txt',header=FALSE)
-x_test = read.table('./test/x_test.txt',header=FALSE)
-y_test = read.table('./test/y_test.txt',header=FALSE)
 features = read.table('./features.txt',header=FALSE)
+subject_train = read.table('./train/subject_train.txt',header=FALSE, col.names = ("sub_id"))
+x_train = read.table('./train/x_train.txt',header=FALSE)
+colnames(x_train) <- features[,2]
+y_train = read.table('./train/y_train.txt',header=FALSE, col.names = ("activity_id"))
+activity_label <- read.table('./activity_labels.txt', header=FALSE, col.names = c("id", "activity_type"))
+subject_test = read.table('./test/subject_test.txt',header=FALSE, col.names = ("sub_id"))
+x_test = read.table('./test/x_test.txt',header=FALSE)
+colnames(x_test) <- features[,2]
+y_test = read.table('./test/y_test.txt',header=FALSE, col.names = ("activity_id"))
 
-x_data <- rbind(x_train, x_test)
-y_data <- rbind(y_train, y_test)
-subject_data <- rbind(subject_train, subject_test)
 
-test = readLines("./features.txt")
-colnames(x_data)= features
+#merge all train datasets, then merge all test datasets
+train_data <- cbind(y_train, subject_train, x_train)
+test_data <- cbind(y_test, subject_test, x_test)
+
+# merge the train and test data sets together
+all_data <- rbind(train_data, test_data)
+
+
+
+
+
+
