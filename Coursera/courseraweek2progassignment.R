90dbd584e62f8fa7afa33e42a8b930e33f75550d
Jules Liebster <jules.liebster@tntp.org>
2017-08-15 12:52:14 -0400

    through step 4 of the coursera programming assignment

--- Coursera/courseraweek2progassignment.R
+++ Coursera/courseraweek2progassignment.R
@@ -23,7 +23,7 @@ subject_train = read.table('./train/subject_train.txt',header=FALSE, col.names =
 x_train = read.table('./train/x_train.txt',header=FALSE)
 colnames(x_train) <- features[,2]
 y_train = read.table('./train/y_train.txt',header=FALSE, col.names = ("activity_id"))
-activity_label <- read.table('./activity_labels.txt', header=FALSE, col.names = c("id", "activity_type"))
+activity_label <- read.table('./activity_labels.txt', header=FALSE, col.names = c("activity_id", "activity_type"))
 subject_test = read.table('./test/subject_test.txt',header=FALSE, col.names = ("sub_id"))
 x_test = read.table('./test/x_test.txt',header=FALSE)
 colnames(x_test) <- features[,2]
@@ -35,10 +35,22 @@ train_data <- cbind(y_train, subject_train, x_train)
 test_data <- cbind(y_test, subject_test, x_test)
 
 # merge the train and test data sets together
-all_data <- rbind(train_data, test_data)
+all_data <- rbind(train_data, test_data) %>%
+  clean_names()
+
 
 # Extract the measurements on the mean and standard deviation for each measurement.
+mean_std_data <- all_data %>%
+  select(contains("mean"),
+         contains("std"),
+         "activity_id",
+         "sub_id")
+
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names.
+mean_std_data <- left_join(mean_std_data, activity_label, by = "activity_id")
 
 
-
-
+# From the data set in step 4, 
+# creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
